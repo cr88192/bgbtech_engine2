425985447cbf89d17b9c08850a1de89f86ae682f
Brendan G Bohannon <cr88192@gmail.com>
2016-05-04 14:40:49 -0500

    Update mm_xi128.c

--- mm/mm_xi128.c
+++ mm/mm_xi128.c
@@ -85,3 +85,83 @@ dctX128 BGBDT_XorXI128(dtcX128 a, dtcX128 b)
   c.d=a.d^b.d;
   return(c);
 }
+
+dctX128 BGBDT_ShlpXI128(dtcX128 a, int b)
+{
+  dtcX128 c;
+  if(!b)
+    return(a);
+  c.a=(a.a<<b);
+  c.b=(a.b<<b)+(a.a>>(32-b));
+  c.c=(a.c<<b)+(a.b>>(32-b));
+  c.d=(a.d<<b)+(a.c>>(32-b));
+  return(c);
+}
+
+dctX128 BGBDT_ShrpXI128(dtcX128 a, int b)
+{
+  dtcX128 c;
+  if(!b)
+    return(a);
+  c.a=(a.a>>b);
+  c.b=(a.b>>b)+(a.a<<(32-b));
+  c.c=(a.c>>b)+(a.b<<(32-b));
+  c.d=(a.d>>b)+(a.c<<(32-b));
+  return(c);
+}
+
+dctX128 BGBDT_ShlXI128(dtcX128 a, int b)
+{
+  dtcX128 a2, c;
+  switch(b>>5)
+  {
+    case 0:
+      a2=a; break;
+    case 1:
+      a2.a=0;    a2.b=a.a;
+      a2.c=a.b;  a2.d=a.d;
+      break;
+    case 2:
+      a2.a=0;    a2.b=0;
+      a2.c=a.a;  a2.d=a.b;
+      break;
+    case 3:
+      a2.a=0; a2.b=0;
+      a2.c=0; a2.d=a.a;
+      break;
+    default:
+      a2.a=0; a2.b=0;
+      a2.c=0; a2.d=0;
+      break;
+  }
+  c=BGBDT_ShlpXI128(a2, b&31);
+  return(c);
+}
+
+dctX128 BGBDT_ShrXI128(dtcX128 a, int b)
+{
+  dtcX128 a2, c;
+  switch(b>>5)
+  {
+    case 0:
+      a2=a; break;
+    case 1:
+      a2.a=a.b;  a2.b=a.b;
+      a2.c=a.c;  a2.d=0;
+      break;
+    case 2:
+      a2.a=a.c;  a2.b=a.d;
+      a2.c=0;    a2.d=0;
+      break;
+    case 3:
+      a2.a=a.d; a2.b=0;
+      a2.c=0;   a2.d=0;
+      break;
+    default:
+      a2.a=0; a2.b=0;
+      a2.c=0; a2.d=0;
+      break;
+  }
+  c=BGBDT_ShrpXI128(a2, b&31);
+  return(c);
+}
